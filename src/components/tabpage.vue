ba6232a9848f8ef0effee8c29d35b6af70852219
Scofieldsu <931180361@qq.com>
2017-07-10 14:21:40 +0800

    change get_all_api to settings

--- src/components/tabpage.vue
+++ src/components/tabpage.vue
@@ -26,7 +26,24 @@
     </el-tab-pane>
     <el-tab-pane label="Get API" name="fouth">
       <div style="height: 700px;overflow: scroll">
-        <el-button @click="getAllApi" type="info" size="small" style="margin: 5px">Get All Api</el-button>
+        <span>
+          <el-button @click="getAllApi" type="info" size="small" style="margin: 5px">Get All Api</el-button>
+          <el-button type="text" @click="dialogVisible = true" style="float: right">Settings</el-button>
+          <el-dialog
+            title="设置"
+            :visible.sync="dialogVisible"
+            size="tiny"
+            :before-close="handleClose">
+            <span>
+              <el-tag type="primary" style="font-size: medium;margin: 5px">请求数据的接口</el-tag>
+              <el-input v-model="data_method" style="width: 50%"></el-input>
+            </span>
+              <span slot="footer" class="dialog-footer">
+                    <el-button @click="dialogVisible = false">取 消</el-button>
+                    <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+              </span>
+          </el-dialog>
+        </span>
         <get-api></get-api>
       </div>
     </el-tab-pane>
@@ -40,27 +57,40 @@
   import resquestArea from './requestbody.vue'
   import getApi from './getapi.vue'
   import moment from 'moment'
+  import ElTag from '../../node_modules/element-ui/packages/tag/src/tag'
+  import ElInput from '../../node_modules/element-ui/packages/input/src/input'
   export default {
     data () {
       return {
-        activeName: 'fouth'
+        activeName: 'fouth',
+        dialogVisible: false,
+        data_method: 'get_all_api'
       }
     },
-    components: {authPage, headerPage, bodyPage, resquestArea, getApi},
+    components: {
+      ElInput, ElTag, authPage, headerPage, bodyPage, resquestArea, getApi},
     methods: {
+      handleClose (done) {
+        this.$confirm('确认关闭？')
+          .then(_ => {
+            done()
+          })
+          .catch(_ => {})
+      },
       handleClick (tab, event) {
         console.log(tab, event)
       },
       getAllApi () {
         let that = this
         let now = new Date()
+        let RpcMethod = this.data_method
         let Resource = {
           'jsonrpc': '2.0',
-          'method': 'get_all_api',
+          'method': RpcMethod,
           'params': {}
         }
         Resource['id'] = moment(now, 'YYYY-MM-DD HH:MM:SS').format()
-        console.log('get all api')
+        console.log(RpcMethod)
         this.$store.commit('newResponse', '')
         this.$store.commit('getallMethods', {
           hello: {
